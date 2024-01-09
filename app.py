7dfc127c48b21262f437e59e476c5366fbc86d1e
Jaime Alonso <jaime.alonso@emtech.digital>
2024-01-09 14:31:57 -0600

    Added the chat model

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import pandas as pd
+from langchain_community.chat_models import ChatOllama
 from langchain_community.document_loaders import CSVLoader
-from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
 
 # Nombre del modelo
 modelo = "phi"
+chat_model = ChatOllama(
+    model=modelo,
+)
 
 # Ubicación del CSV
 csv_filepath = './data/listings.csv'
@@ -16,7 +20,9 @@ data = loader.load()
 
 # Crear el vectorstore
 embedding_model = OllamaEmbeddings(model=modelo)
-db = Chroma.from_documents(data, embedding_model, persist_directory="./chroma_db")
+# db = Chroma.from_documents(data, embedding_model, persist_directory="./chroma_db")
+db = Chroma(persist_directory="./chroma_db",
+            embedding_function=embedding_model)
 
 # Crear el retriever
 retriever_tool = db.as_retriever()
